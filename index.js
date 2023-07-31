3b60f4ab438c4256c863c12ebb60115b47fa4ef5
Akash <phillo@Akashs-MacBook-Air.local>
2023-07-31 18:39:51 +0530

    Response Body

--- index.js
+++ index.js
@@ -20,102 +20,31 @@ app.use(bodyParser.json());   // to parse the body while reciving from the clien
 
 
 app.get("/", (req, res) => {
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Landing Page</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                line-height: 1.6;
-                margin: 0;
-                padding: 0;
-            }
-    
-            header {
-                background-color: #333;
-                color: #fff;
-                padding: 1rem;
-                text-align: center;
-            }
-    
-            main {
-                max-width: 800px;
-                margin: 0 auto;
-                padding: 2rem;
-            }
-    
-            h1 {
-                font-size: 2.5rem;
-                margin-bottom: 1.5rem;
-            }
-    
-            p {
-                font-size: 1.1rem;
-                margin-bottom: 1.5rem;
-            }
-    
-            .cta-button {
-                display: inline-block;
-                background-color: #007BFF;
-                color: #fff;
-                padding: 1rem 2rem;
-                text-decoration: none;
-                font-size: 1.2rem;
-                border-radius: 5px;
-                transition: background-color 0.3s ease;
-            }
-    
-            .cta-button:hover {
-                background-color: #0056b3;
-            }
-    
-            footer {
-                background-color: #f4f4f4;
-                padding: 1rem;
-                text-align: center;
-            }
-        </style>
-    </head>
-    <body>
-        <header>
-            <h1>Welcome to Our Landing Page</h1>
-        </header>
-    
-        <main>
-            <h1>Your Catchy Headline Here</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec faucibus mauris. Proin vel orci sed arcu
-                accumsan fringilla. In euismod faucibus urna, eu rhoncus quam.</p>
-            <p>Suspendisse semper euismod eros nec hendrerit. Curabitur nec iaculis orci. Proin varius dictum mi quis
-                ultricies. Nulla facilisi.</p>
-            <a href="/fib" class="cta-button">Get Started</a>
-        </main>
-    
-        <footer>
-            <p>&copy; 2023 YourCompany. All rights reserved.</p>
-        </footer>
-    </body>
-    </html>
-    `)
+    res.status(200).sendFile(`${__dirname}/landingPage.html`, (err) => {
+        console.log(err);
+    })
 });
 
 const sum1ToN = (count) => {
     let sum = 0;
-    for(let i=0; i<=count; i++) {
+    let product = 1;
+    for(let i=1; i<=count; i++) {
         sum += i;
+        product *= i;
     }
-    return sum;
+    const body = {sum, product}
+    return body;
 }
 
-app.get('/getSum', (req, res) => {
-    // const counter = req.body.counter;
-    // const sum = sum1ToN(counter);
+
+app.post('/getSum', (req, res) => {
+    const {counter} = req.body;
+    const response = sum1ToN(counter);
     // res.send(`Um from One to ${n}: ${sum}`);
-    const sum = sum1ToN(100);
-    const print = "the sum from 1 to 100 is " + sum;
-    res.send(`<h2>${print}</h2>`);
+    // const sum = sum1ToN(100);
+    // const print = "the sum from 1 to 100 is " + sum;
+    // res.send(`<h2>${print}</h2>`);
+    res.status(200).send(response);
 });
 
 const fibonaic = (n) => {
@@ -143,6 +72,12 @@ app.get('/fib', (req, res) => {
     const fibre = fibonaic(count);
     res.send(`${fibre}`);
 });
+
+app.get("/html", (req, res) => {
+    res.sendFile(`${__dirname}/index.html`, (err) => {
+        console.log(err);
+    })
+})
 
 const greetList = {
     "english": "hello",
@@ -262,7 +197,7 @@ const updateLang = (req, res) => {
 }
 
 const deleteLang = (req, res) => {
-    const {lang} = req.query;
+    const {lang} = req.body;
     if(getGreet(lang)) {
         delete greetList[lang];
         return res.status(200).send("Deleted the lang :)");
