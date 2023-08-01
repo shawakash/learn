6430e074d68d3fc3f3b36341245484424b7a6f2a
Akash <phillo@Akashs-MacBook-Air.local>
2023-08-01 15:35:34 +0530

    POST request in goLang

--- fetch.go
+++ fetch.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-const API_END_POINT = "http://localhost:3000/getSum?counter=5";
+type Data struct {
+	Counter int `json:"counter"`
+}
+
+
+const API_END_POINT = "http://localhost:3000/getSum";
 
 func getHttpRequest() ([]byte, error) {
 	response, err := http.Get(API_END_POINT);
@@ -24,16 +31,71 @@ func getHttpRequest() ([]byte, error) {
 
 }
 
-func main() {
-	data, err := getHttpRequest();
+func postRequest() {
 
-	if err != nil {
-		fmt.Println("Error response from the request :(");
-		fmt.Println(err);
-		return;
+	// Create a JSON object to be sent in the request body
+	data := Data{
+		Counter: 10,
 	}
-	fmt.Println("http Response :)");
-	fmt.Println(string(data));
+
+	// Convert the data to a JSON byte slice
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+	// fmt.Println(string(jsonData))  // json object
+
+	// Create a new HTTP request with the JSON data as the request body
+	req, err := http.NewRequest("POST", API_END_POINT, bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	// Set the Content-Type header to specify that we are sending JSON data
+	req.Header.Set("Content-Type", "application/json")
+
+	// Create an HTTP client and send the request
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Check the response status code and process the response body as needed
+	if resp.StatusCode == http.StatusOK {
+		// Process the successful response
+		// ...
+		var responseJSON map[string]interface{}
+		err := json.NewDecoder(resp.Body).Decode(&responseJSON)
+		if err != nil {
+			fmt.Println("Error decoding response JSON:", err)
+			return
+		}
+		fmt.Println(responseJSON)
+	} else {
+		// Handle other response codes or errors
+		// ...
+	}
+}
+
+
+
+func main() {
+	// data, err := getHttpRequest();
+
+	// if err != nil {
+	// 	fmt.Println("Error response from the request :(");
+	// 	fmt.Println(err);
+	// 	return;
+	// }
+	// fmt.Println("http Response :)");
+	// fmt.Println(string(data));
+
+	postRequest();
 	return;
 
 }
